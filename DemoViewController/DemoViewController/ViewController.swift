a706a93f097ccdb615175af628c55120475fb2bd
up1 <somkiat.p@gmail.com>
2019-06-25 23:52:40 +0700

    Start demo project to load ViewController

---
+++ DemoViewController/DemoViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoViewController
+//
+//  Created by Somkiat Puisungnoen on 25/6/2562 BE.
+//  Copyright © 2562 Somkiat Puisungnoen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
