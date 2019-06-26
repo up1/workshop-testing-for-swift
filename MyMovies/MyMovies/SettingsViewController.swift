4ca6d0acad719f25ef92b8da7432f4aa5048d6b6
up1 <somkiat.p@gmail.com>
2019-06-27 00:05:56 +0700

    04-second-test-case-pass

---
+++ MyMovies/MyMovies/SettingsViewController.swift
@@ -0,0 +1,7 @@
+import UIKit
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
+
--- MyMovies/MyMoviesTests/SettingsTests.swift
+++ MyMovies/MyMoviesTests/SettingsTests.swift
@@ -4,17 +4,20 @@ import XCTest
 class SettingsTests: XCTestCase {
 
     func test_title_is_Settings() {
-        // Arrange
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        
-        // Act
-        let vc = storyboard.instantiateViewController(withIdentifier: "Settings")
-        
-        let _ = vc.view
-        
-        // Assert
+        let vc = settingsViewController()
         XCTAssertEqual(vc.navigationItem.title, "Settings")
-        
+    }
+    
+    func test_label_text_is_Number_of_results_to_display() {
+        let vc = settingsViewController()
+        XCTAssertEqual(vc.label.text!, "Number of results to display")
+    }
+    
+    func settingsViewController() -> SettingsViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "Settings") as! SettingsViewController
+        let _ = vc.view
+        return vc
     }
 
 }
