3130bcd86be7ec3b3e4db5429523865b1bb41f9d
up1 <somkiat.p@gmail.com>
2019-06-26 23:04:21 +0700

    01 Start workshop

---
+++ MyMovies/MyMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Somkiat Puisungnoen on 26/6/2562 BE.
+//  Copyright © 2562 Somkiat Puisungnoen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
